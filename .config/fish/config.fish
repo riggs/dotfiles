e54fbc46cb883736df85aaf2aaf36e040a432834
Benjamin Riggs <deisum@gmail.com>
2021-12-05 03:02:41 -0600

    Can't have fish_variables in git because it contains the prompt, which changes with every command.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,5 @@ set -gx GNUARMEMB_TOOLCHAIN_PATH (realpath (realpath (which arm-none-eabi-gcc)/.
 set -gx PYTHONSTARTUP ~/.pythonrc.py
 
 if status is-interactive
-    set -x PATH $fish_user_paths
+    set -gx PATH /Users/riggs/Library/Python/3.9/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Users/riggs/.local/bin
 end
