ec18b6a39c78adce9b8e8bc49492e43ab32cbff1
Benjamin Riggs <deisum@gmail.com>
2021-12-06 22:55:31 -0600

    Added global git ignore, also switched from tide prompt because it doesn't work with CLion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
+set -gx PATH /Users/riggs/Library/Python/3.9/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Users/riggs/.local/bin
 set -gx ZEPHYR_TOOLCHAIN_VARIANT gnuarmemb
-set -gx GNUARMEMB_TOOLCHAIN_PATH (realpath (realpath (which arm-none-eabi-gcc)/../..))
+set -gx GNUARMEMB_TOOLCHAIN_PATH /Applications/ARM
 set -gx PYTHONSTARTUP ~/.pythonrc.py
 
 if status is-interactive
-    set -gx PATH /Users/riggs/Library/Python/3.9/bin /opt/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /Users/riggs/.local/bin
 end
