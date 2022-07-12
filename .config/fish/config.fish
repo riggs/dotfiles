1044face0b987419e6bb2d93ec07e6700f311cf6
Benjamin Riggs <deisum@gmail.com>
2022-07-12 13:50:13 -0500

    Use single mvim window for CLI-opned files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,4 +4,19 @@ set -gx GNUARMEMB_TOOLCHAIN_PATH /Applications/ARM
 set -gx PYTHONSTARTUP ~/.pythonrc.py
 
 if status is-interactive
+    function mvim
+        for arg in $argv
+            switch $arg
+            case {-,+}'*'
+                set --append opts $arg
+            case '*'
+                set --append files $arg
+            end
+        end
+        if [ -n "$files" ]
+            set --prepend files --remote-tab-silent
+        end
+        /Applications/MacVim.app/Contents/bin/mvim --servername VIM $opts $files &
+        disown
+    end
 end
---
+++ .profile
@@ -0,0 +1,4 @@
+
+
+# Added by Toolbox App
+export PATH="$PATH:/Users/riggs/Library/Application Support/JetBrains/Toolbox/scripts"
